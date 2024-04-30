f1decc00a942819e7cabcc72810b1bedf721433a
shri-rang <jangamshrirang003@gmail.com>
2024-04-30 12:49:45 +0530

    delete and put opration done

--- methods/post-request.js
+++ methods/post-request.js
@@ -1,2 +1,24 @@
-module.exports = (req, res)=>{
+const bodyParser = require("../util/body-parser");
+const crypto = require("crypto");
+const writeToFile = require('../util/write-to-file');
+module.exports = async(req, res)=>{
+
+        if(req.url === "/api/movies"){
+                    try {
+                        let body = await bodyParser(req) 
+                        body.id = crypto.randomUUID();
+                        req.movies.push(body);
+                        writeToFile(req.movies);
+                        res.writeHead(201,{"Content-Type" : "application/json"});
+                         console.log("Request body", body);
+                        res.end();
+                        
+                    } catch (error) {
+                        res.writeHead( 400,{"Content-Type": "application/json"});
+                        res.end(JSON.stringify({title:"Validation Failed",  message:"Request body not valid"}));   
+                    } 
+        } else{
+                 res.writeHead( 404,{"Content-Type": "application/json"});
+                res.end(JSON.stringify({title:"Not Found",  message:"Route Not Found"}));   
+        }
 }
